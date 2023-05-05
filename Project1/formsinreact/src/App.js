7cdb51f80563e678070b2402d0ea60be4d9e04b2
SUMEG-04 <sumeg04112001@gmail.com>
2023-05-05 13:54:31 +0530

    new project

--- Project1/formsinreact/src/App.js
+++ Project1/formsinreact/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App=()=>{
+
+  const [name,newName]=useState("");
+  const [showName,getName]=useState();
+
+  const setName=()=>{
+    getName(name);
+  };
+
+  const eventCall=(event)=>{
+    newName(event.target.value);
+  }
+  
+  return(
+    <>
+      <div>
+        <h1>Hello {showName}</h1>
+        <input type='text' placeholder='Enter your name' onChange={eventCall} value={name}/>
+        <button onClick={setName}>Click me</button>
+      </div>
+    </>
   );
 }
 
